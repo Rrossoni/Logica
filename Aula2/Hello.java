a84ca679e0616269c3a731409b0c1414d5c8aacd
Rrossoni <57258482+Rrossoni@users.noreply.github.com>
2020-07-23 21:52:18 -0300

    Add files via upload

---
+++ Aula2/Hello.java
@@ -0,0 +1,12 @@
+/**
+      Hello World
+      @author Ricardo Rossoni 
+      */
+
+      public class Hello {
+public static void main(String[] args){
+   System.out.println( "Hello World");  
+}
+
+
+      }
